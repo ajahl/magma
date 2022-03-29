d81f4d25b9f473ad3c94102e981f2f9d3c873183
Cameron Voisey <cameron.voisey@tngtech.com>
2022-03-29 15:15:19 +0200

    Fix mutex errors in metricsd/client_api.go and tenants/client_api.go
    Signed-off-by: Cameron Voisey <cameron.voisey@tngtech.com>

--- orc8r/cloud/go/services/tenants/client_api.go
+++ orc8r/cloud/go/services/tenants/client_api.go
@@ -17,6 +17,7 @@ import (
 	"context"
 
 	"github.com/golang/glog"
+	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -85,17 +86,18 @@ func GetTenant(ctx context.Context, tenantID int64) (*tenant_protos.Tenant, erro
 	return tenant, nil
 }
 
-func SetTenant(ctx context.Context, tenantID int64, tenant tenant_protos.Tenant) error {
+func SetTenant(ctx context.Context, tenantID int64, tenant *tenant_protos.Tenant) error {
 	oc, err := getTenantsClient()
 	if err != nil {
 		return err
 	}
 
+	tenantCopy := proto.Clone(tenant).(*tenant_protos.Tenant)
 	_, err = oc.SetTenant(
 		ctx,
 		&tenant_protos.IDAndTenant{
 			Id:     tenantID,
-			Tenant: &tenant,
+			Tenant: tenantCopy,
 		},
 	)
 	if err != nil {
@@ -133,13 +135,14 @@ func GetControlProxy(ctx context.Context, tenantID int64) (*tenant_protos.GetCon
 	return controlProxy, nil
 }
 
-func CreateOrUpdateControlProxy(ctx context.Context, controlProxy tenant_protos.CreateOrUpdateControlProxyRequest) error {
+func CreateOrUpdateControlProxy(ctx context.Context, controlProxy *tenant_protos.CreateOrUpdateControlProxyRequest) error {
 	oc, err := getTenantsClient()
 	if err != nil {
 		return err
 	}
 
-	_, err = oc.CreateOrUpdateControlProxy(ctx, &controlProxy)
+	controlProxyCopy := proto.Clone(controlProxy).(*tenant_protos.CreateOrUpdateControlProxyRequest)
+	_, err = oc.CreateOrUpdateControlProxy(ctx, controlProxyCopy)
 	if err != nil {
 		return mapErr(err)
 	}
