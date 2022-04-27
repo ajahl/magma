f91dc49b821ce2cf8128e83aa4eb254e26fb0393
Alex Jahl <alexander.jahl@tngtech.com>
2022-04-27 18:03:23 +0200

    testing(orc8r): clean up
    Signed-off-by: Alex Jahl <alexander.jahl@tngtech.com>

--- orc8r/cloud/go/test_utils/unit_test_proto_utils.go
+++ orc8r/cloud/go/test_utils/unit_test_proto_utils.go
@@ -15,10 +15,10 @@ package test_utils
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 	"testing"
 
-	protoV1 "github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/proto"
 )
@@ -40,39 +40,59 @@ func AssertErrorsEqual(t *testing.T, expectedError, actualError string) {
 }
 
 // AssertMapsEqual compares two maps of with proto messages.
-func AssertMapsEqual(t *testing.T, expected, actual map[string]protoV1.Message) {
-	assert.Equal(t, len(expected), len(actual))
-	for key := range actual {
-		AssertMessagesEqual(t, protoV1.MessageV2(expected[key]), protoV1.MessageV2(actual[key]))
+func AssertMapsEqual(t *testing.T, expected, actual interface{}) {
+	expValue, actValue := reflect.ValueOf(expected), reflect.ValueOf(actual)
+	if ekind, akind := expValue.Kind(), actValue.Kind(); ekind != akind || ekind != reflect.Map {
+		assert.Equal(t, expected, actual)
+	}
+	assert.Equal(t, expValue.Len(), actValue.Len())
+	for _, key := range expValue.MapKeys() {
+		AssertMessagesEqual(t, expValue.MapIndex(key).Interface(), actValue.MapIndex(key).Interface())
 	}
 }
 
-// AssertMessagesEqual compares two proto.Message's with proto.Equal.
+// AssertListsEqual compares two lists of proto messages.
+func AssertListsEqual(t *testing.T, expected, actual interface{}) {
+	expValue, actValue := reflect.ValueOf(expected), reflect.ValueOf(actual)
+	if ekind, akind := expValue.Kind(), actValue.Kind(); ekind != akind || ekind != reflect.Slice {
+		assert.Equal(t, expected, actual)
+	}
+	assert.Equal(t, expValue.Len(), actValue.Len())
+	for i := 0; i < expValue.Len(); i++ {
+		AssertMessagesEqual(t, expValue.Index(i).Interface(), actValue.Index(i).Interface())
+	}
+}
+
+// AssertMessagesEqual compares two proto.Messages with proto.Equal.
 // Prints string representations of messages upon inequality.
 // Note, that string representations provided by protobuf are not stable.
 // Please only pay attention to the field values themselves, not to whitespaces
 // in between.
-func AssertMessagesEqual(t *testing.T, expected, actual proto.Message) bool {
-	equal := proto.Equal(expected, actual)
-	if !equal {
+func AssertMessagesEqual(t *testing.T, expected, actual interface{}) {
+	expMsg, expOk := expected.(proto.Message)
+	actMsg, actOk := actual.(proto.Message)
+	if !expOk || !actOk {
+		assert.Equal(t, expected, actual)
+	} else if !proto.Equal(expMsg, actMsg) {
 		assert.Fail(t, fmt.Sprintf("Not equal: \n"+
 			"expected: %+v\n"+
-			"actual: %+v\n"+
+			"actual  : %+v\n"+
 			"Note, that string representations provided by protobuf are not guaranteed to be stable and the comparison between messages does not rely on them. Please pay attention to differences in the field values only.",
-			expected, actual))
+			expMsg, actMsg))
 	}
-	return true
 }
 
-// AssertMessagesNotEqual compares two proto.Message's with proto.Equal.
+// AssertMessagesNotEqual compares two proto.Messages with proto.Equal.
 // Fails if messages are equal and prints string representations of messages.
 // Note, that string representations provided by protobuf are not stable.
 // Please only pay attention to the field values themselves, not to whitespaces
 // in between.
-func AssertMessagesNotEqual(t *testing.T, expected, actual proto.Message) bool {
-	equal := proto.Equal(expected, actual)
-	if equal {
+func AssertMessagesNotEqual(t *testing.T, expected, actual interface{}) {
+	expMsg, expOk := expected.(proto.Message)
+	actMsg, actOk := actual.(proto.Message)
+	if !expOk || !actOk {
+		assert.NotEqual(t, expected, actual)
+	} else if proto.Equal(expMsg, actMsg) {
 		assert.Fail(t, "Messages are equal but should be different.")
 	}
-	return true
 }
